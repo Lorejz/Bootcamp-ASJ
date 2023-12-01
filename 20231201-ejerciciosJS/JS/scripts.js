9404daba1d226a5d1b85b192a4daf0c5b94d592e
Lorejz <juanjdominguez17@gmail.com>
2023-12-01 12:58:06 -0300

    nuevo ejercicios JavaScript

---
+++ 20231201-ejerciciosJS/JS/scripts.js
@@ -0,0 +1,52 @@
+
+// Ejercicio "Cantidad De Caracteres"
+ //var mensajeEjercicio_1 = "Ingrese un texto";
+ //var inputTexto = prompt(mensajeEjercicio_1); //input usuario 
+ //console.log(inputTexto.length); //print en consola de cantidad de caracteres
+
+// Ejercicio "Calculadora De Edad"
+ //var mensajeEjercicio_2 = "Ingrese su edad";
+ //var inputEdad = prompt(mensajeEjercicio_2); //input usuario
+ //var calculoDias = inputEdad * 365.
+
+ //alert("Tiene "+calculoDias+" dias");
+
+// Ejercicio "Suma De Valores"
+ //var mensajeEjercicio_3_1 = "Ingrese un valor, num1"
+ //var mensajeEjercicio_3_2 = "Ingrese un valor, num2"   
+ //var inputNum1 = parseInt(prompt(mensajeEjercicio_3_1)); //input num1
+ //var inputNum2 = parseInt(prompt(mensajeEjercicio_3_2)); //input num2
+ //var resultado = inputNum1 + inputNum2;
+
+ //alert("El resultado de num1 y num2 es: "+resultado);
+
+// Ejercicio "Calculador de abastecimiento de por vida"
+ // Ejercicio 1
+    //var miEdad = 23;
+    //var miEdadMaxima = 85;
+    //var snackFavorito = "Chetos";
+    //var snackXdia = 5;
+    //var snackQueComere = (miEdadMaxima-miEdad)*snackXdia
+
+   // alert("Comere "+snackQueComere+" "+snackFavorito+" antes de morir")
+ 
+ // Ejercicio 2
+ var cantDiasVacas = 15;
+ var presupuestoVacas = 150000;
+ var comida = 30;
+ var presupuestoXcomida = presupuestoVacas/(cantDiasVacas*comida)
+
+ alert("Podés gastar "+presupuestoXcomida+" pesos en cada comida para que te alcance la plata durante los "+cantDiasVacas+" días de viaje.");
+ 
+
+
+
+
+
+
+
+
+
+// var/let/const nombre_variable;
+
+//utilizar camel case, ej: listadoClientes

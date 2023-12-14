92adbd085886b97c0fc38ff1f9c894f136ef7051
Lorejz <juanjdominguez17@gmail.com>
2023-12-14 01:07:43 -0300

    crud hecho faltan filtros

--- 20231212-Angular/miApp/src/app/app.module.ts
+++ 20231212-Angular/miApp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +21,7 @@ import { MainSectionComponent } from './components/main-section/main-section.com
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgModule,
+    CommonModule,
     FormsModule
   ],
   providers: [],
--- 20231212-Angular/miApp/src/app/components/main-section/main-section.component.ts
+++ 20231212-Angular/miApp/src/app/components/main-section/main-section.component.ts
@@ -7,6 +7,50 @@ import { Component } from '@angular/core';
 })
 export class MainSectionComponent {
 
-  valor_input = '';
+  tituloInput = '';
+  descripcionInput = '';
+  tareas: Tarea[] = [];
+
+  filtro: 'todos' | 'finalizados' | 'eliminados' = 'todos';
+
+  agregarTarea(){
+/*     let tarea : Tarea = new Tarea(this.tituloInput,this.descripcionInput);
+    this.tareas.push(tarea); */
+    this.tareas.unshift(new Tarea(this.tituloInput,this.descripcionInput));
+    this.tituloInput = '';
+    this.descripcionInput = '';
+
+    console.log(this.tituloInput);
+  }
+  
+  eliminarTarea(index:number){
+    this.tareas[index].eliminado=true;
+  }
+  finalizarTarea(index:number){
+    this.tareas[index].finalizado=true;
+  }
+
+  cambiarFiltro(filtro: 'todos' | 'finalizados' | 'eliminados') {
+    
+  }
+
+
+
+}
+
+
+class Tarea {
+
+  titulo: string;
+  descripcion: string;
+  finalizado: boolean;
+  eliminado:  boolean;
+
+  constructor(titulo: string,descripcion: string){
+    this.titulo = titulo;
+    this.descripcion = descripcion;
+    this.finalizado = false;
+    this.eliminado = false;
+  };
 
 }

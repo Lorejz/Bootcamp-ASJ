1b0e324c51a20fd63601fa04e80d7732ccd0d267
Lorejz <juanjdominguez17@gmail.com>
2024-01-15 12:33:43 -0300

    ejercicios java

---
+++ 20240111-Java/Prueba_1/src/MiClase.java
@@ -0,0 +1,131 @@
+import java.util.Scanner;
+
+public class MiClase {
+
+	public static void main(String[] args) {
+//		System.out.println("Hola mundo como estan estoy aprendiendo backend");
+//		System.out.println("aprendiendo atajos"); //ctrl + barra espaciadora sysout
+//		int edad;
+//		char letra;
+//		double num1;
+//		boolean estado;
+//		String nombre;
+//		float precio;
+//		//buena practica usar los wrappers String 
+//		
+//		edad = 25;
+//		letra = 'C';
+//		num1 = 123.4;
+//		nombre = "Juan Jose";
+//		precio= 999;
+//		
+//		System.out.println("La edad es "+(edad + 1));
+//		System.out.println(num1);
+//		System.out.println(nombre);
+//		System.out.println(precio);
+//		
+//		if(edad > 18) {
+//			System.out.println("es mayor");
+//		}
+//		Scanner scanner = new Scanner(System.in);
+//		int myInt_1 = scanner.nextInt();
+//		int myInt_2 = scanner.nextInt();
+//		int myInt_3 = scanner.nextInt();
+//		scanner.close();
+//
+//		System.out.println("\n"+myInt_1);
+//		System.out.println(myInt_2);
+//		System.out.println(myInt_3);
+
+		
+//		Scanner scanner = new Scanner(System.in);
+//		
+//		int myInt = scanner.nextInt();
+//		//scanner.nextLine();
+//		
+//	    double myDouble = scanner.nextDouble();
+//	    scanner.nextLine();
+//	    
+//	    String myString = scanner.nextLine();
+//	    
+//	    scanner.close();
+//	    
+//	    System.out.println("String: " + myString);
+//	    System.out.println("Double: " + myDouble);
+//	    System.out.println("Int: " + myInt);
+		
+		
+		//Scanner scanner = new Scanner(System.in);
+		
+	    
+//        if (scanner.hasNextInt()) {
+//            int myInt = scanner.nextInt();
+//            
+//            if(myInt >= 2 && myInt <= 5) {
+//            	System.out.println("Not Weird");
+//            }
+//            if(myInt >= 6 && myInt <= 20 ) {
+//            	System.out.println("Weird");
+//            }
+//            if(myInt > 20) {
+//            	System.out.println("Not Weird");
+//            }
+//        } else {
+//            // si no es de tipo entero, mostrar mensaje
+//            System.out.println("Weird");
+//        }
+//        scanner.close();
+		
+//		Scanner scanner = new Scanner(System.in);
+//		int n = scanner.nextInt();
+//		scanner.close();
+//		
+//		if (n >= 2 && n <= 20) {
+//			for (int i = 1; i <= 10; i++) {
+//				int result = n * i;
+//				System.out.println(n+" x "+ i +" = " + result);
+//			}
+//		}else {
+//			System.out.println("Ingrese un numero valido de 2 hasta 20");
+//		}
+		
+//        Scanner scanner = new Scanner(System.in);
+//
+//        // Read the number of queries
+//        int q = scanner.nextInt();
+//
+//        // Process each query
+//        for (int i = 0; i <= q; i++) {
+//            // Read values for a, b, and n
+//            int a = scanner.nextInt();
+//            int b = scanner.nextInt();
+//            int n = scanner.nextInt();
+//
+//            // Print the series for the current query
+//            printSeries(a, b, n);
+//        }
+
+        // Close the scanner
+        //scanner.close();      
+	    
+	}
+	
+	
+//	private static void printSeries(int a, int b, int n) {
+//        int term = a;
+//
+//        // Print the first n terms of the series
+//        for (int i = 0; i < n; i++) {
+//            term += Math.pow(2, i) * b;
+//            System.out.print(term + " ");
+//        }
+//
+//        // Move to the next line after printing the series
+//        System.out.println();
+//    }
+
+	
+	
+	
+	
+}
